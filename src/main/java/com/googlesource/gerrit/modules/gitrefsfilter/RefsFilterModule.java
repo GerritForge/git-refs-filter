f681b01839e5a80ba6ac3d1d0da4cf3163a439a2
Luca Milanesio <luca.milanesio@gmail.com>
2018-12-11 16:26:45 +0000

    Initial commit of the closed refs filtering module
    The new refsfilter Gerrit module allows to reduce the footprint
    of the Git refs advertizing phase by hiding all the refs associated
    to non-live changes.
    
    Typical scenario is a CI build system: as closed changes (merged, abandoned)
    are not live anymore and would never be built, they are filtered out of
    the Git advertizing phase, saving a lot of processing from both client
    and server side.
    
    Another typical scenario is Gerrit slave replication for development
    purposes. Typically you do not work anymore on closed changes and
    there is little or even no value in getting those refs advertized
    and replicated to remote sites.
    
    Change-Id: I4636bc79bfc7b3290c4d491a638cacc8318d43a7

---
+++ src/main/java/com/googlesource/gerrit/modules/gitrefsfilter/RefsFilterModule.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.modules.gitrefsfilter;
+
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.extensions.config.CapabilityDefinition;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+
+public class RefsFilterModule extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    install(
+        new FactoryModuleBuilder()
+            .implement(WithUserWrapper.class, WithUserWrapper.class)
+            .build(WithUserWrapper.Factory.class));
+
+    install(
+        new FactoryModuleBuilder()
+            .implement(ForProjectWrapper.class, ForProjectWrapper.class)
+            .build(ForProjectWrapper.Factory.class));
+
+    bind(PermissionBackend.class).to(RefsFilterPermissionBackend.class).in(Scopes.SINGLETON);
+
+    bind(CapabilityDefinition.class)
+        .annotatedWith(Exports.named(FilterRefsCapability.HIDE_CLOSED_CHANGES_REFS))
+        .to(FilterRefsCapability.class)
+        .in(Scopes.SINGLETON);
+  }
+}
