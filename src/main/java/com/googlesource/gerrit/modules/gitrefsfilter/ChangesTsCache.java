bf5dbefbeb2ed98836828f56fa5de16f23c19c08
Luca Milanesio <luca.milanesio@gmail.com>
2022-01-11 01:04:46 +0000

    Introduce change-ts cache for showing changes just merged
    Many CI/CD systems perform validation also on changes just
    merged as post-merge build. Keep the refs of the changes
    still visible for a grace time for allowing them to be
    fetched by builders.
    
    The grace time is fixed to 24h: the configurability
    is addressed on the follow-up change.
    
    Change-Id: I478c6f287a90cad2acc32187316789cd5463513c

---
+++ src/main/java/com/googlesource/gerrit/modules/gitrefsfilter/ChangesTsCache.java
@@ -0,0 +1,82 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.modules.gitrefsfilter;
+
+import com.google.common.cache.CacheLoader;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+
+public class ChangesTsCache {
+  public static final String CHANGES_CACHE_TS = "changes_ts";
+
+  private final Provider<ReviewDb> dbProvider;
+
+  @Inject
+  public ChangesTsCache(Provider<ReviewDb> dbProvider) {
+    this.dbProvider = dbProvider;
+  }
+
+  public static Module module() {
+    return new CacheModule() {
+      @Override
+      protected void configure() {
+        cache(CHANGES_CACHE_TS, ChangeCacheKey.class, new TypeLiteral<Long>() {})
+            .loader(Loader.class);
+      }
+    };
+  }
+
+  @Singleton
+  static class Loader extends CacheLoader<ChangeCacheKey, Long> {
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+    private final ChangeNotes.Factory changeNotesFactory;
+    private final ChangesTsCache changesTsCache;
+
+    @Inject
+    Loader(ChangeNotes.Factory changeNotesFactory, ChangesTsCache changesTsCache) {
+      this.changeNotesFactory = changeNotesFactory;
+      this.changesTsCache = changesTsCache;
+    }
+
+    @Override
+    public Long load(ChangeCacheKey key) throws Exception {
+      try {
+        Long res =
+            changeNotesFactory
+                .createChecked(
+                    changesTsCache.dbProvider.get(),
+                    key.repo(),
+                    key.project(),
+                    key.changeId(),
+                    key.changeRevision())
+                .getChange()
+                .getLastUpdatedOn()
+                .getTime();
+        return res;
+      } catch (NoSuchChangeException e) {
+        logger.atFine().withCause(e).log(
+            "Change %d does not exist: returning zero epoch", key.changeId());
+        return 0L;
+      }
+    }
+  }
+}
